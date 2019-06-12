05245d0c0afd7a9077d61c718b90118693c5bdb6
Joseph Star <lolobaby12@hotmail.com>
2019-06-12 11:40:25 +0300

    project

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 http.createServer().listen(port);
 
 const bot = new discord.Client();
+const embed = new discord.RichEmbed();
 
 const streamOptions = { seek: 0, volume: 1 };
 
@@ -18,7 +19,6 @@ var datas={
     musicTitles:[],
     numUrls:0,
     skip: false,
-    url:undefined,
 }
 bot.on('ready', ()=>{
     console.log('biri beni mi çağırdı');
@@ -47,28 +47,27 @@ bot.on('message',async msg=>{
             for(i=0;i<videos.length;i++){
                  response[i] = `\n${i+1}) ${videos[i].title}`;
             }
-            const embed = new discord.RichEmbed();
             embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
             embed.setDescription(response);
             msg.channel.send(embed);
 
             msg.channel.send('**HANGİSİNİ SEÇEYİM?**');
+            // if(isNaN(msg.content)) return;
             const filter = m =>!isNaN(m.content) && m.content>0 && m.content<videos.length+1;
             const collector = msg.channel.createMessageCollector(filter);
             collector.videos=videos;
+
             collector.once('collect',function(m){
-                datas.url=this.videos[parseInt(m.content)-1].url;
-                datas.url="https://www.youtube.com"+datas.url;
+                let url=this.videos[parseInt(m.content)-1].url;
+                url="https://www.youtube.com"+url;
                 let Title=this.videos[parseInt(m.content)-1].title;
                 console.log(Title);
-                play(datas.url,Title);
+                play(url,Title);
             })
-            console.log(datas.url);
         });
         async function play(url,Title){
         datas.musicTitles.push(Title);
         datas.VoiceChannel = msg.guild.channels.find(channel => channel.id ==='509009367065034775');
-        // console.log(datas.url);
         if(ytdl.validateURL(url)){
             console.log('valid url');
             var flag = datas.musicUrls.some(element=>element===url);
@@ -76,6 +75,7 @@ bot.on('message',async msg=>{
                 datas.musicUrls.push(url);
                 datas.numUrls++;
                 if(datas.VoiceChannel!=null){
+
                     if(datas.VoiceChannel.connection){
                         console.log('connection exist');
                         const embed = new discord.RichEmbed();
@@ -86,45 +86,97 @@ bot.on('message',async msg=>{
                     else{
                         try{
                             datas.voiceConnection= await datas.VoiceChannel.join();
-                            await playSong(msg.channel,datas.voiceConnection,datas.VoiceChannel);
+                            console.log("bot müzik çalmak için katıldı");
+                            await playSong(msg.channel,datas.voiceConnection,datas.VoiceChannel,0);
+                            embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
+                            embed.setDescription(`Şuan çalan şarkı ${datas.musicTitles[0]}`);
+                            msg.channel.send(embed);
                         }catch(ex){
                             console.log(ex);
                         }
                     }
-                }
+                }else console.log("bağlantı yok");
             }
         }}
     }
     if(msg.content.split(' ')[0]==='!skip'){
         datas.skip=true;
-        playSong(msg.channel,datas.voiceConnection,datas.VoiceChannel);
+        var a=parseInt(result);
+        if(!isNaN(a) && a!=1){
+            console.log(!isNaN(a));
+            embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
+            embed.setDescription(`Geçilen şarkı: ${datas.musicTitles[a-1]}`);
+            if(datas.numUrls>0) msg.channel.send(embed);
 
+            datas.musicTitles.splice(a-1,1);
+            datas.musicUrls.splice(a-1,1);
+            datas.numUrls--;
+        }else {
+            playSong(msg.channel,datas.voiceConnection,datas.VoiceChannel,0);
+            embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
+            embed.setDescription(`Şuan çalan şarkı ${datas.musicTitles[0]}`);
+            if(datas.numUrls>0) msg.channel.send(embed);
+            else msg.channel.send("Tüm şarkılar bitti dostum.");
+        }
     }
     if(msg.content.split(' ')[0]==='!playlist'){
-
         var embedList = datas.musicTitles.slice();
-        for(i=0;i<datas.numUrls;i++){
+        for(i=0;i<datas.numUrls;i++)
             embedList[i]=`\n${i+1}) ${embedList[i]}`;
-            
-        }
-        const embed = new discord.RichEmbed();
+        
         embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
         embed.setDescription(`**ŞARKI LİSTESİ**\n${embedList}`);
         msg.channel.send(embed);
 
     }
+    if(msg.content.split(' ')[0]==='!help'){
+        embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
+        embed.setDescription(`**KOMUTLAR**\n 
+        1)!state => botun durumunu değiştirir.(idle,dnd,onlinde,invisible)\n
+        2)!setGame => botun oynadığı oyunu değiştirir.(istediğin herşey :D)\n
+        3)!play => youtube şarkı araması yapar ardından istediğin şarkının numarasını gir.\n
+        4)!playlist => oynatma listesini gösterir.\n
+        5)!skip => istenilen şarkıyı geçer.\n
+        6)!reset => botu resetler.\n
+        7)!hot => yazı tura.\n
+        8)!dice => 2'li zar atar.`);
+        msg.channel.send(embed);
+    }
+    if(msg.content.split(' ')[0]==='!dice'){
+        var dice1 = Math.round((Math.random()*5)+1);
+        var dice2 = Math.round((Math.random()*5)+1);
+        
+        msg.channel.send(`Zarlarrrrrr! ${dice1} ile ${dice2} geldi.`);
+    }
+    if(msg.content.split(' ')[0]==='!hot'){
+        var sayi = Math.round(Math.random());
+        if(sayi==1) a='TURA';
+        else a='YAZI';
+        msg.channel.send(a);
+    }
+    if(msg.content.split(' ')[0]==='!reset'){
+        playSong(msg.channel,datas.voiceConnection,datas.VoiceChannel,1);
+    }
+    // if(msg.content.split(' ')[0]==='!set'){
+    //     var user=msg.content.split(" ").splice(1,1);//dizi
+    //     var input=msg.content.split(" ").slice(2,10).join(" ");//string
+    //     console.log(GuildMember.nickname);
+    //     console.log(user,input);
+    // }         WIP
+    
 })
 bot.on('error', err=>{
     console.log(err);
 })
-async function playSong(messageChannel,voiceConnection,VoiceChannel){
+async function playSong(messageChannel,voiceConnection,VoiceChannel,reset){
+    if(reset){
+        voiceConnection.disconnect();
+        datas.numUrls=0;
+        datas.musicTitles=[];
+        datas.musicUrls=[];
+    }
     const stream=ytdl(datas.musicUrls[0],{filter: 'audioonly'});
     const dispatcher = voiceConnection.playStream(stream,streamOptions);
-
-    const embed = new discord.RichEmbed();
-    embed.setAuthor(bot.user.username,bot.user.displayAvatarURL);
-    embed.setDescription(`Şuan çalan şarkı ${datas.musicUrls[0]}`);
-    // msg.channel.send(embed);
 
     if(datas.skip===true){
         console.log("Şarkı sonlandı");
@@ -134,13 +186,23 @@ async function playSong(messageChannel,voiceConnection,VoiceChannel){
 
     dispatcher.on('end',()=>{
 
-    if(datas.musicUrls.length-1==0)
+    if(datas.musicUrls.length-1==0){
+        console.log("bütün şarkılar bitti.");
+        msg.channel.send("Tüm şarkılar bitti dostum.");
         VoiceChannel.leave();
-    else{
         datas.musicUrls.shift();
+        datas.musicTitles.shift();
+        datas.numUrls--;
+    }
+
+    else{
+        console.log("sonraki şarkıya geçiliyor.");
+        datas.musicUrls.shift();
+        datas.musicTitles.shift();
+        datas.numUrls--;
         setTimeout(()=>{
-            playSong(messageChannel,voiceConnection,VoiceChannel);
-        },1000);
+            playSong(messageChannel,voiceConnection,VoiceChannel,0);
+        },2000);
     }
     })
 }
